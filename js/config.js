8c286bcaaec3586b510d639e80cb72d9abba8473
Aurea <147183938+aureafin@users.noreply.github.com>
2026-09-24 21:40:32 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,2 +1,9 @@
-// Firebase Console > Configurações do projeto > Os teus apps > Web (</>) > "firebaseConfig". Estes valores são públicos; a segurança vem de firestore.rules e storage.rules.
-window.CONFIG = { FIREBASE: { apiKey: '', authDomain: '', projectId: '', storageBucket: '', messagingSenderId: '', appId: '' } };
+// Dados públicos do projeto Firebase (curso-4398c). A segurança vem de firestore.rules e storage.rules.
+window.CONFIG = { FIREBASE: {
+  apiKey: "AIzaSyDjnCcmGTcb8kBA9y5hHAI64UKgu_9ViXw",
+  authDomain: "curso-4398c.firebaseapp.com",
+  projectId: "curso-4398c",
+  storageBucket: "curso-4398c.firebasestorage.app",
+  messagingSenderId: "283337218930",
+  appId: "1:283337218930:web:0ac539dd227d08d08bdbeb"
+} };
