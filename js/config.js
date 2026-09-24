a37e70f5c02d4f0b9ba80ccaa950d634f227f463
Aurea <147183938+aureafin@users.noreply.github.com>
2026-09-24 21:30:28 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,17 @@
-// Firebase Console > Configurações do projeto > Os teus apps > Web (</>) > "firebaseConfig". Estes valores são públicos; a segurança vem de firestore.rules e storage.rules.
-window.CONFIG = { FIREBASE: { apiKey: '', authDomain: '', projectId: '', storageBucket: '', messagingSenderId: '', appId: '' } };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjnCcmGTcb8kBA9y5hHAI64UKgu_9ViXw",
+  authDomain: "curso-4398c.firebaseapp.com",
+  projectId: "curso-4398c",
+  storageBucket: "curso-4398c.firebasestorage.app",
+  messagingSenderId: "283337218930",
+  appId: "1:283337218930:web:0ac539dd227d08d08bdbeb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
